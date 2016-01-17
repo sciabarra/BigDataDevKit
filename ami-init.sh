3f4c47c217fde8413f7b561bc160267c4908b35a
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-01-17 08:04:35 +0100

    ami-init fixed

--- ami-init.sh
+++ ami-init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # change password here
 echo "ec2-user:changeme" | chpasswd
-yum -y update && yum -y install docker git
+# end chages
+yum -y update && yum -y install docker git nginx
 service docker start
 sed  -i -e 's/PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 service sshd restart
-docker run --name term -p "8080:8080" -dt nathanleclaire/wetty app.js --port 8080 --sshhost $(hostname -I | awk '{ print $1}') --sshuser ec2-user
+mkdir /etc/cert
+printf "\\n\\n\\n\\n\\n\\n\\n" | openssl req -x509 -newkey rsa:2048 -keyout /etc/cert/cert.key -out /etc/cert/cert.pem -days 30000 -nodes
+docker run --name term --rm=true -p "1443:1443" -v /etc/cert:/etc/cert -u term -ti nathanleclaire/wetty app.js --port 1443 --sshhost $(hostname -I | awk '{ print $1}') --sshuser ec2-user --sslkey /etc/cert/cert.key --sslcert /etc/cert/cert.pem
