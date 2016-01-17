25e295e30cb99d73277b210609dbbd7c6c82069f
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-01-17 08:50:45 +0100

    update script - adding a pad line at the end

--- ami-init.sh
+++ ami-init.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # change password here
 echo "ec2-user:changeme" | chpasswd
-# end chages
+# end changes
 yum -y update && yum -y install docker git nginx
 service docker start
 sed  -i -e 's/PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 service sshd restart
 mkdir /etc/cert
 printf "\\n\\n\\n\\n\\n\\n\\n" | openssl req -x509 -newkey rsa:2048 -keyout /etc/cert/cert.key -out /etc/cert/cert.pem -days 30000 -nodes
+docker pull nathanleclaire/wetty
 docker run --name term --rm=true -p "1443:1443" -v /etc/cert:/etc/cert -u term -ti nathanleclaire/wetty app.js --port 1443 --sshhost $(hostname -I | awk '{ print $1}') --sshuser ec2-user --sslkey /etc/cert/cert.key --sslcert /etc/cert/cert.pem
+# end script
