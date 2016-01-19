d19f7d117904a0d25b0bc4325ba71099a6b960e1
root <root@ip-172-31-22-168.ec2.internal>
2016-01-19 15:32:45 +0000

    big refactoring - not tested yet

--- build.sh
+++ build.sh
@@ -6,12 +6,9 @@ then echo "Please type password for devenv"
 fi
 test -e base-ssh/id_rsa ||  ssh-keygen -t rsa -f base-ssh/id_rsa -N '' 
 cp base-ssh/id_* devenv
-docker build -t shaz/1-java base-java
-docker build -t shaz/1-ssh base-ssh
-docker build -t shaz/1-hadoop base-hadoop
-docker build -t shaz/1-spark base-spark 
-docker build -t shaz/1-zeppelin base-zeppelin
-docker build -t shaz/1-intellij base-intellij
-docker build -t shaz/1-wettynovnc base-wettynovnc
-docker images | grep shaz/1
-docker-compose build
+for i in \
+    base-java base-ssh base-hadoop hadoop \
+    zeppelin sparkjobserver  \
+    base-scala devenv 
+do docker build -t bddk/$i $i || exit 1
+done
---
+++ dropbox.sh
@@ -0,0 +1,4 @@
+wget --no-check-certificate -O/usr/bin/dropbox https://www.dropbox.com/download?dl=packages/dropbox.py 
+chmod +x /usr/bin/dropbox
+wget --no-check-certificate -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+R
