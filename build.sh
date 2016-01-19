52d77136675e29cb5de61ef46fa42384fe202310
Michele Sciabarra <michele+youtrack@sciabarra.com>
2016-01-19 21:35:43 +0000

    reorg layout to save files

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if "$USER" != "app"
+then echo "Please run this as app user"
+fi
 if ! test -e devenv/_password 
 then echo "Please type password for devenv"
      read PASSWORD
@@ -6,9 +9,16 @@ then echo "Please type password for devenv"
 fi
 test -e base-ssh/id_rsa ||  ssh-keygen -t rsa -f base-ssh/id_rsa -N '' 
 cp base-ssh/id_* devenv
+mkdir -p $HOME/Dropbox/IdeaProjects $HOME/Dropbox/zeppelin/notebook
 for i in \
     base-java base-ssh base-hadoop hadoop \
     zeppelin sparkjobserver  \
     base-scala base-novnc devenv 
 do docker build -t bddk/$i $i || exit 1
 done
+if ! test -e $HOME/dropbox.py ; then
+wget --no-check-certificate -O$HOME/dropbox.py https://www.dropbox.com/download?dl=packages/dropbox.py 
+chmod +x $HOME/dropbox.py
+echo Please configure Dropbox to save your data using $HOME/dropbox.py
+fi
+
